171c5c7b6e450c0d781e1776d8ee7295f0ce45ba
Saulius Kazokas <saulens22@gmail.com>
2022-07-15 14:50:46 +0300

    Add livewire

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Livewire\TestComponent;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/livewire-test', TestComponent::class)->name('livewire-test');
