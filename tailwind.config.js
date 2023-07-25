137837dc8ce5cc6d9a782392a37b266dd5a03885
Saulius Kazokas <saulens22@gmail.com>
2023-07-25 21:52:12 +0300

    v10 upgrade

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,6 @@ module.exports = {
     content: [
         './resources/**/*.blade.php',
         './resources/**/*.js',
-        './vendor/wireui/wireui/resources/**/*.blade.php',
-        './vendor/wireui/wireui/ts/**/*.ts',
-        './vendor/wireui/wireui/src/View/**/*.php'
-    ],
-    presets: [
-        require('./vendor/wireui/wireui/tailwind.config.js')
     ],
     theme: {
         extend: {},
